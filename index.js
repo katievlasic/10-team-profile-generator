fa55dedc0fbc967c2834e2258b20a892d99d9864
katievlasic <118415802+katievlasic@users.noreply.github.com>
2023-02-18 20:17:02 -0600

    update .gitignore and build CLI in index.js for inputs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require("fs"); // fs = file system
+const inquirer = require("inquirer"); // module package
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
@@ -8,3 +10,53 @@ Employee.prototype.printCard = function () {
         `Name: ${this.name}\n ID: ${this.ID}\n `
       )
 }
+
+// Function to write HTMLfile
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("Sucess!")
+    );
+  };
+
+function init() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the name of the Employee?",
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "What is the role of this Employee?",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern",
+            ],
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is the Employee's github?",
+          },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the Employee's email?",
+          },
+        ])
+          .then((answers) => {
+            console.log(answers);
+            // const data = generateMarkdown(answers);
+            // writeToFile('./dist/README.md', data);
+          }).catch((error) => {
+            if (error) {
+              console.log(error);
+            }
+          });
+      };
+
+// Function call to initialize app
+init();
