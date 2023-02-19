ab9380e6ca0afc4e5e689ef38dd3a90fea2ae352
katievlasic <118415802+katievlasic@users.noreply.github.com>
2023-02-18 20:00:25 -0600

    setup .js files and mock .html file

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+Employee.prototype.printCard = function () {
+    console.log(
+        `Name: ${this.name}\n ID: ${this.ID}\n `
+      )
+}
