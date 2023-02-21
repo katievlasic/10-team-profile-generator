58ef4fc91a859fb906b56269fabf86d4d023d4f0
katievlasic <118415802+katievlasic@users.noreply.github.com>
2023-02-20 19:09:27 -0600

    finalize Employee.test.js

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -4,10 +4,30 @@ test("name can be set using a constructor", () => {
   const employee = new Employee("Katie", 1, "test@gmail.com"); // create new 'dummy' data
   expect(employee.name).toBe("Katie");
 });
+test("id should be a number", () => {
+  const employee = new Employee("Katie", 1, "test@gmail.com"); // create new 'dummy' data
+  expect(employee.id).toBe(1);
+});
+test("email should contain an @ symbol", () => {
+  const employee = new Employee("Katie", 1, "test@gmail.com"); // create new 'dummy' data
+  expect(employee.email).toBe("test@gmail.com");
+});
 
-test("method test", () => {
+test("getName method test", () => {
   const employee = new Employee("Katie", 1, "test@gmail.com"); // create new 'dummy' data
   expect(employee.getName()).toBe("Katie");
+});
+test("getRole method test", () => {
+  const employee = new Employee("Katie", 1, "test@gmail.com"); // create new 'dummy' data
+  expect(employee.getRole()).toBe("employee");
+});
+test("getId method test", () => {
+  const employee = new Employee("Katie", 1, "test@gmail.com"); // create new 'dummy' data
+  expect(employee.getId()).toBe(1);
+});
+test("getEmail method test", () => {
+  const employee = new Employee("Katie", 1, "test@gmail.com"); // create new 'dummy' data
+  expect(employee.getEmail()).toBe("test@gmail.com");
 });
 
 // remove const { default: test } = require('node:test');
