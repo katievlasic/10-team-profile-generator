460dc32256da32f3f44dca155c2eacb6d9a167ab
katievlasic <118415802+katievlasic@users.noreply.github.com>
2023-02-20 19:13:01 -0600

    finalize Engineer.test.js

---
+++ tests/Engineer.test.js
@@ -0,0 +1,27 @@
+const Engineer = require("../lib/Engineer"); // need class name capitalized and ../ jumps up a directory
+
+test("name can be set using a constructor", () => {
+  const employee = new Engineer("Katie", 1, "test@gmail.com","katievlasic"); // create new 'dummy' data
+  expect(employee.name).toBe("Katie");
+});
+test("id can be set using a constructor", () => {
+  const employee = new Engineer("Katie", 1, "test@gmail.com","katievlasic"); // create new 'dummy' data
+  expect(employee.id).toBe(1);
+});
+test("email can be set using a constructor", () => {
+  const employee = new Engineer("Katie", 1, "test@gmail.com","katievlasic"); // create new 'dummy' data
+  expect(employee.email).toBe("test@gmail.com");
+});
+test("github can be set using a constructor", () => {
+  const employee = new Engineer("Katie", 1, "test@gmail.com","katievlasic"); // create new 'dummy' data
+  expect(employee.github).toBe("katievlasic");
+});
+
+test("getRole method test", () => {
+    const employee = new Engineer("Katie", 1, "test@gmail.com","katievlasic"); // create new 'dummy' data
+    expect(employee.getRole()).toBe("Engineer");
+  });
+test("getGithub method test", () => {
+    const employee = new Engineer("Katie", 1, "test@gmail.com","katievlasic"); // create new 'dummy' data
+    expect(employee.getGithub()).toBe("katievlasic");
+  });
